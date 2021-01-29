6489ae10a5abb75e3ba79c74a9f93c9c9f6baabc
Aakash Mishra <amishra@college.harvard.edu>
2021-01-29 17:09:35 -0500

    update basis

--- hw1/T1_P4.py
+++ hw1/T1_P4.py
@@ -7,6 +7,7 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 csv_filename = 'data/year-sunspots-republicans.csv'
 years  = []
@@ -58,7 +59,36 @@ X = np.vstack((np.ones(years.shape), years)).T
 # The shape of arrays you return should be: (a) 24x6, (b) 24x12, (c) 24x6, (c) 24x26
 # xx is the input of years (or any variable you want to turn into the appropriate basis).
 def make_basis(xx,part='a'):
-    return None
+    if part == 'a':
+        result = np.array([])
+        for val in xx:
+            arr = [val**e for e in range(1,6)]
+            arr.insert(0, 1)
+            np.append(result, [arr], axis=0)
+        return result
+    elif part == 'b':
+        result = np.array([])
+        for val in xx:
+            arr = [math.exp((-(val - e)**2)/25) for e in range(1960,2015,5)]
+            arr.insert(0, 1)
+            np.append(result, [arr], axis=0)
+        return result
+    elif part == 'c':
+        result = np.array([])
+        for val in xx:
+            arr = [math.cos(val/e) for e in range(1,6)]
+            arr.insert(0, 1)
+            np.append(result, [arr], axis=0)
+        return result
+    else:
+        result = np.array([])
+        for val in xx:
+            arr = [math.cos(val/e) for e in range(1,26)]
+            arr.insert(0, 1)
+            np.append(result, [arr], axis=0)
+        return result
+
+
 
 # Nothing fancy for outputs.
 Y = republican_counts
@@ -67,6 +97,28 @@ Y = republican_counts
 def find_weights(X,Y):
     w = np.linalg.solve(np.dot(X.T, X) , np.dot(X.T, Y))
     return w
+
+def find_residual_values(X, Y, w):
+    return np.sum(np.square(Y - np.dot(w,X)))
+
+X1_basis = make_basis(years, part='a')
+X2_basis = make_basis(years, part='b')
+X3_basis = make_basis(years, part='c')
+X4_basis = make_basis(years, part='d')
+
+w1 = find_weights(X1_basis, republican_counts)
+w2 = find_weights(X2_basis, republican_counts)
+w3 = find_weights(X3_basis, republican_counts)
+w4 = find_weights(X4_basis, republican_counts)
+
+residuals = []
+
+residuals.append(find_residual_values(X1_basis, republican_counts, w1))
+residuals.append(find_residual_values(X2_basis, republican_counts, w2))
+residuals.append(find_residual_values(X3_basis, republican_counts, w3))
+residuals.append(find_residual_values(X4_basis, republican_counts, w4))
+
+
 
 # Compute the regression line on a grid of inputs.
 # DO NOT CHANGE grid_years!!!!!
